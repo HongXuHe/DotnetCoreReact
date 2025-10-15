96e91c2f21ec93266a57eda9ac499d27b5d1ce7d
HongXuHe <matt917917@gmail.com>
2025-10-15 19:40:12 +1100

    setup api project

---
+++ server/Server/Server.API/Controllers/BasicController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BasicController:ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("test");
+    }
+}
